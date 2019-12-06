57c377fc75648d48d68794769c0f3ee8b940e7e7
Eduard Abrakov <abrakovem@mail.ru>
2019-12-06 17:49:57 +0500

    all done

--- target/classes/static/js/main.js
+++ target/classes/static/js/main.js
@@ -1,14 +1,65 @@
-$(function() {
-    var now = new Date();
-    var format_time= now.toLocaleTimeString("ru");
-    var format_date = now.toLocaleDateString("ru");
+var idTask = null;
+var format_time = new Date().toLocaleTimeString("ru");
+var format_date = new Date().toLocaleDateString("ru");
 
+//open form edit-task
+function editTask(id) {
+    $('#replace-form').css('display','flex');
+    idTask = id;
+}
+//Getting task
+function getTask(id) {
+    var link = $('.record-' + id +' .task-link');
+    if(document.getElementsByClassName("desc-id-"+ id).length === 0) {
+        $.ajax({
+            method: "GET",
+            url: "/tasks/" + id,
+            success: function (response) {
+                var code = '<span class= "desc-id-' + id + '"> Описание:'
+                    + response.description + format_date + format_time + '</span>';
+                link.parent().append(code);
+
+            },
+            error: function (response) {
+                if (response.status == 404) {
+                    alert('Задача не найдена!');
+                }
+            }
+
+        });
+    }
+    return false
+}
+
+//Edit task
+function saveEditTask() {
+    let form = $('#form-task2');
+    let nameVal = form.find('input[name="editName"]').val();
+    let descriptionVal = form.find('input[name="editDescription"]').val();
+    let url = '/tasks/' + idTask;
+    let jsonString2 = JSON.stringify({id : idTask, name : nameVal, description : descriptionVal});
+    debugger;
+    $.ajax({
+        method: "PUT",
+        url: url,
+        data: jsonString2,
+        contentType: 'application/json',
+        success: function(response) {
+            var oldName = $('.record-' + idTask + ' .task-link');
+            var oldDescription = $('.record-' + idTask +' .desc-id-' + idTask);
+            oldName.text(nameVal);
+            oldDescription.text(descriptionVal);
+        }
+    });
+    return true;
+}
+
+$(function() {
     const appendTask = function(data) {
 
-        var taskCode = '<a href="#" class="task-link" data-id="' + data.id + '">' + 'Задача: ' + data.name
-            + '</a>  ' +
-            '<a href="#" class="task-edit" data-id="' + data.id +'">edit</a>  ' +
-            '<a href="#" class="task-del" data-id="' + data.id + '">delete</a>';
+        var taskCode = '<a href="#" class="task-link" data-id="' + data.id +'" onclick="getTask(' + data.id + ')">' + 'Задача: ' + data.name + '</a>  ' +
+            '<a href="#" class="task-edit" data-id="' + data.id +'" onclick="editTask(' + data.id + ')"> edit </a>' +
+            '<a href="#" class="task-del" data-id="' + data.id + '"> delete </a>';
         $('#task-list')
             .append('<div class="record-' + data.id + '">' + taskCode + '</div>');
     };
@@ -56,66 +107,11 @@ $(function() {
         return false;
     });
 
-    //open form edit-task
-    $(document).on('click','.task-edit', function () {
-        $('#replace-form').css('display','flex');
-    });
-
     //close form edit-task
     $('.btn2').click(function () {
         $('.form-replace').css('display','none');
     });
 
-    //Edit task
-    $('#save-replaceTask').click(function() {
-        let form = $('#form-task2');
-        let nameVal = form.find('input[name="editName"]').val();
-        let descriptionVal = form.find('input[name="editDescription"]').val();
-        var taskId = $('.task-edit').data('id');
-        var id = $('.record-'+ taskId +' .task-link').data("id");
-        let url = '/tasks/' + id;
-        let jsonString2 = JSON.stringify({name : nameVal, description : descriptionVal});
-        debugger;
-        $.ajax({
-            method: "PUT",
-            url: url,
-            data: jsonString2,
-            contentType: 'application/json',
-            success: function(response) {
-                var oldName = $('.record-' + taskId + ' .task-link');
-                var oldDescription = $('.record-' + taskId +' .desc-id-' + taskId);
-                oldName.text(nameVal);
-                oldDescription.text(descriptionVal);
-                appendTask(data);
-            }
-        });
-        return false;
-    });
-
-    //Getting task
-    $(document).on('click','.task-link', function () {
-        var link = $(this);
-        var taskId = link.data('id');
-        if(document.getElementsByClassName("desc-id-"+ taskId).length === 0) {
-            $.ajax({
-                method: "GET",
-                url: "/tasks/" + taskId,
-                success: function (response) {
-                    var code = '<span class= "desc-id-' + taskId + '"> Описание:'
-                        + response.description + " " + format_date + " " + format_time + '</span>';
-                    link.parent().append(code);
-
-                },
-                error: function (response) {
-                    if (response.status == 404) {
-                        alert('Задача не найдена!');
-                    }
-                }
-
-            });
-        }
-        return false
-    });
 
     //Delete task
     $(document).on('click', '.task-del', function(){
