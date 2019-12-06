c9ab7d26aa51783190ffb784e57f8707d9d2bcc6
Eduard Abrakov <abrakovem@mail.ru>
2019-12-06 22:42:54 +0500

    add sql database

--- src/main/java/main/TaskController.java
+++ src/main/java/main/TaskController.java
@@ -1,4 +1,6 @@
 package main;
+import main.model.ToDoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -6,40 +8,50 @@ import org.springframework.web.bind.annotation.*;
 import main.model.Task;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class TaskController {
 
+    @Autowired
+    private ToDoRepository toDoRepository;
+
     @GetMapping("/tasks/")
     public List<Task> list() {
-        return TaskStorage.getAllTasks();
+     Iterable<Task> iterable = toDoRepository.findAll();
+     List<Task> tasks = new ArrayList<>();
+     for (Task task : iterable) {
+         tasks.add(task);
+     }
+        return tasks;
     }
 
     @PostMapping("/tasks/")
     public int add (@Valid @RequestBody Task task) {
-
-        return TaskStorage.addTask(task);
+        return toDoRepository.save(task).getId();
     }
 
     @GetMapping("/tasks/{id}")
     public ResponseEntity getTask(@PathVariable int id) {
-        Task task = TaskStorage.getTask(id);
-        if (task == null) {
+        Optional<Task> optionalTask = toDoRepository.findById(id);
+        if (!optionalTask.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
-        return new ResponseEntity(task, HttpStatus.OK);
+        return new ResponseEntity(optionalTask.get(), HttpStatus.OK);
     }
 
     @DeleteMapping("/tasks/{id}")
     public ResponseEntity deleteTask(@PathVariable int id) {
-        TaskStorage.deleteTask(id);
+        toDoRepository.deleteById(id);
         return ResponseEntity.status(HttpStatus.OK).body(null);
     }
 
     @PutMapping("/tasks/{id}")
     public ResponseEntity<Task> edit(@Valid @RequestBody Task task, @PathVariable int id) {
-        Task newTask = TaskStorage.editTask(id,task);
+        toDoRepository.deleteById(id);
+         Task newTask = toDoRepository.save(task);
         if (newTask == null) {
             return ResponseEntity.notFound().build();
         }
